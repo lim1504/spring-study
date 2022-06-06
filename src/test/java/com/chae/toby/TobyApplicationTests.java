834b18dcf8d2b9f9d27670651e469796b4b35afe
cslim <lim4158@gmail.com>
2022-06-06 17:46:02 +0900

    first commit

---
+++ src/test/java/com/chae/toby/TobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chae.toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
